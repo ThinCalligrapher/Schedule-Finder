00b997d29a414b7c5e8e4632a4da33f99bbffc86
ThinCalligrapher <chadleino97@gmail.com>
2025-11-25 14:56:10 -0600

    Bubble sort from memory

---
+++ files/binary.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+
+
+int main() {
+    // lets make our 2 arrays, get the addresses and iterators through them
+        // then (they arrays are sorted already) we do binary search on them
+
+
+    int arr[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    int unsorted[10] {8, 6, 4, 2, 0, 9, 7, 5, 3, 1};
+
+    int *p1 = &arr[0];
+
+    int *p2 = &arr[9];
+
+    
+}
